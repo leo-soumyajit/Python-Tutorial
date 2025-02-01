8d231e78151d33763674d102b757f6adaecde206
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-01 16:14:22 +0530

    Variables datatypes number string boolean noneType List tupple and dict

---
+++ Basic Of Python/variables.py
@@ -0,0 +1,18 @@
+a = 10
+print(type(a),a)
+b = True
+print(type(b),b)
+c = "Soumyajit"
+print(type(c),c)
+d = None
+print(type(d),d)
+e = complex(8,2)
+print(type(e),e)
+list = [1,2,3,[4,5,6],["Kanu","titu"],["mota"]]
+print(list)
+
+tuple = (1,2,3,("kanu","titu"))
+print(tuple)
+
+dict = {"num":1,"name":"kanu","canVote":False}
+print(dict)
