1dd6a170599f9edea1cfa51a8afe2809d929fb9e
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-01 16:29:52 +0530

    Python Arithmatic Operators

---
+++ Basic Of Python/ArithmaticOperators.py
@@ -0,0 +1,10 @@
+#Python Arithmatic Operators
+a=10
+b=5
+print("Addition is : ",a+b)
+print("Subtraction is : ",a-b)
+print("Multiplication is : ",a*b)
+print("Division is : ",a/b)
+print("Modulo is : ",a%b)
+print("Floor Value is : ",15//6) #floor value is like round of value 15/6 = 2.5 if we need only integer type we use // which is like round of
+print(5**5)
