a2d77664ebd28cf32f9d61fcad313e280ea1a2f2
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-01 15:18:54 +0530

    separator and end parameter in print statement and escape sequence

--- First.py
+++ First.py
@@ -1,4 +1,6 @@
 
 a=19
 b=10
-print("Programming ki maki Chut ",a*10*b)
+print("Programming is d love ",a*10*b)
+print ("hei im a \"Good boy\"\nthe viewers also good boy/girls") #escape sequence
+print("Hello Guys",10,11,sep=' ',end="End here") # separator and end parameter in print statement
