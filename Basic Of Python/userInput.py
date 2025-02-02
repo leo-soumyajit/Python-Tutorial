3f7e14392b36d03183ae200a0069838a8c8e56b3
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-02 15:29:58 +0530

    Taking User input in Python Programming Language

---
+++ Basic Of Python/userInput.py
@@ -0,0 +1,18 @@
+#using Variable
+a = input("Enter Your Name : ")
+print("My Name is :",a)
+
+#Directly from print function()
+print("My Name is :",input("Enter Your Name : "))
+
+"""
+    By default input func() takes String type
+    we have to typeCast it into int type or anything else
+    by using int() ,float() , .... etc
+"""
+#syntax
+x = int(input("Enter 1st num : "))
+y = int(input("Enter 2nd num : "))
+print("Sum of two number is :",x+y)
+
+
