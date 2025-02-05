2440225587b7cf3022a1f471cca945a0870bacb1
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-05 23:16:13 +0530

    Conditional Statement (if , elif , else)

---
+++ Basic Of Python/if-else.py
@@ -0,0 +1,34 @@
+#conditional operators
+#ex 1
+age = int(input("Enter your age : "))
+if(age>=80):
+    print("U should not drive")
+elif(age>=18):
+    print("u can drive")
+else:
+    print("u can drive")
+
+#ex 2
+num = int(input("Enter your number"))
+if(num>0):
+    print("This is a Positive Number")
+elif(num==0):
+    print("This is zero")
+else:
+    print("This is a Negative Number")
+
+#ex 3 (nested if)
+num = int(input("Enter your number"))
+if(num>0):
+    if(num<=10):
+        print("Number is bw 1-10")
+    elif(num>10 and num<=20):
+        print("Number is bw 11-20")
+    else:
+        print("Number greater than 20")
+elif(num==0):
+    print("Number is zero")
+else:
+    print("number is negative")
+
+
