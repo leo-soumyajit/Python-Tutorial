cc214ff52464c31c1cf309d5462e3e31fc3c8ae9
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-08 22:11:47 +0530

    user define Function in Python Programming

---
+++ Basic Of Python/functionsInPython.py
@@ -0,0 +1,17 @@
+def calculateGmean(a,b):
+    res = (a*b)/(a+b)
+    return res
+
+print(calculateGmean(9,8))
+
+def greaterNumber(a,b):
+    if a>b:
+        print(a,"is Greater..")
+    else:
+        print(b, "is Greater..")
+
+greaterNumber(10,11)
+
+def isLesser(a,b):
+    pass    # says to python the impl of this function will do later
+            # otherwise it gives an exception
