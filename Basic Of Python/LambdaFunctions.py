bf9ca85bfab01a637c00f38f983629ea56af36ed
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-27 13:16:21 +0530

    lambda function in python

---
+++ Basic Of Python/LambdaFunctions.py
@@ -0,0 +1,14 @@
+#Lamda functions
+
+# def double(x):
+#     return x*2
+# print(double(5))
+
+double = lambda x:x*2
+print(double(6))
+
+avg = lambda x,y: (x+y)/2
+print(avg(10,5))
+
+var = lambda x,y:print(x*y)
+var(10,10)
