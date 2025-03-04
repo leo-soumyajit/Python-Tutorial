42ce8e062d3c8b90e8dc8a384837d23c4cb67dda
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-03-04 15:25:05 +0530

    Getter and Setter

---
+++ OOPs/getter&setter.py
@@ -0,0 +1,35 @@
+from tkinter.font import names
+
+
+class student:
+    def __init__(self,name,roll):
+        self.name=name
+        self.roll=roll
+
+    @property
+    def name_getter(self):
+        return self.name
+
+    @name_getter.setter
+    def name_setter(self,name):
+        self.name = name
+
+    @property
+    def roll_getter(self):
+        return self.roll
+
+    @roll_getter.setter
+    def roll_setter(self,roll):
+        self.roll = roll
+
+
+    def info(self):
+        print(self.name_getter,self.roll_getter)
+
+
+
+obj = student("Soumya","166")
+obj.name_setter="Soumyajit"
+obj.roll_setter ="166"
+print(obj.name_getter)
+obj.info()
